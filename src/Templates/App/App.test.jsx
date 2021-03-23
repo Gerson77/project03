2c8439d9d03c7f104b0b529fa65d3d7c9f43429d
Gerson77 <gerson.snts55@gmail.com>
2021-03-23 12:02:48 -0300

    update

--- src/Templates/App/App.test.jsx
+++ src/Templates/App/App.test.jsx
@@ -5,7 +5,7 @@ import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
   const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', { name: 'Hello Word!' })
+  const headingContainer = screen.getByRole('heading', { name: 'Hello' })
     .parentElement;
   expect(headingContainer).toHaveStyleRule({
     background: theme.colors.mainBg,
