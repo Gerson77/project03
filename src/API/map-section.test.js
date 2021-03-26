383bf4c946c6cd5ed00229bcdd9ceed38b2ead35
Gerson77 <gerson.snts55@gmail.com>
2021-03-26 19:04:12 -0300

    Limpando testes

---
+++ src/API/map-section.test.js
@@ -0,0 +1,184 @@
+import {
+  mapSectionContent,
+  mapSections,
+  mapSectionTwoColumns,
+  mapTextGrid,
+  mapImageGrid,
+} from './map-section';
+
+import pagesFakeData from './dados.json';
+
+describe('map-sections', () => {
+  it('should render predefined section if no data', () => {
+    const data = mapSections();
+    expect(data).toEqual([]);
+  });
+
+  it('should render sections wiht correct data', () => {
+    const data = mapSections(pagesFakeData[0].sections);
+    expect(data[1].component).toBe('section.section-two-columns');
+  });
+
+  it('should test section with invalid data ', () => {
+    const withNoTextImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+      },
+    ]);
+    const withNoComponent = mapSections([{}]);
+    expect(withNoTextImageGrid).toEqual([
+      {
+        __component: 'section.section-grid',
+      },
+    ]);
+    expect(withNoComponent).toEqual([{}]);
+  });
+
+  it('should test section.section-grid with no text_grid or image_grid', () => {
+    const withNoTextImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+        image_grid: [{}],
+      },
+      {
+        __component: 'section.section-grid',
+        text_grid: [{}],
+      },
+    ]);
+    expect(withNoTextImageGrid.length).toBe(2);
+  });
+
+  it('should map two columns', () => {
+    const data = mapSectionTwoColumns();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+  it('should map two columns', () => {
+    const data = mapSectionTwoColumns({
+      __component: 'section.section-two-columns',
+      title: 'title',
+      description: 'abc',
+      metadata: {
+        background: true,
+        section_id: 'contact',
+      },
+      image: {
+        url: 'a.svg',
+      },
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('contact');
+    expect(data.srcImg).toBe('a.svg');
+    expect(data.text).toBe('abc');
+    expect(data.title).toBe('title');
+  });
+
+  it('should map section content with no data', () => {
+    const data = mapSectionContent();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.html).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section content', () => {
+    const data = mapSectionContent({
+      __component: 'section.section-content',
+      title: 'Pricing',
+      content: 'Hello',
+      metadata: {
+        background: false,
+        section_id: 'pricing',
+      },
+    });
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-content');
+    expect(data.sectionId).toBe('pricing');
+    expect(data.html).toBe('Hello');
+    expect(data.title).toBe('Pricing');
+  });
+
+  it('should map grid text with data', () => {
+    const data = mapTextGrid({
+      __component: 'section.section-grid',
+      _id: '60594bb14bb4da1d002d9bd5',
+      title: 'My Grid',
+      description: 'Hello',
+      text_grid: [
+        {
+          title: 'Teste 1',
+          description: 'Lorem ipsum',
+        },
+        {
+          title: 'Teste 2',
+          description: 'Lorem ipsum',
+        },
+      ],
+      image_grid: [],
+      metadata: {
+        background: true,
+        section_id: 'grid-one',
+      },
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-grid-text');
+    expect(data.sectionId).toBe('grid-one');
+    expect(data.title).toBe('My Grid');
+    expect(data.description).toBe('Hello');
+    expect(data.grid[0].title).toBe('Teste 1');
+    expect(data.grid[0].description).toBe('Lorem ipsum');
+  });
+
+  it('should map grid text without data', () => {
+    const data = mapTextGrid(undefined);
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-grid-text');
+    expect(data.sectionId).toBe('');
+    expect(data.title).toBe('');
+    expect(data.description).toBe('');
+  });
+
+  it('should map grid image without data', () => {
+    const data = mapImageGrid(undefined);
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-grid-image');
+    expect(data.sectionId).toBe('');
+    expect(data.title).toBe('');
+    expect(data.description).toBe('');
+  });
+
+  it('should map grid image with data', () => {
+    const data = mapImageGrid({
+      __component: 'section.section-grid',
+      title: 'Gallery',
+      description: 'Lorem',
+      text_grid: [],
+      image_grid: [
+        {
+          image: {
+            alternativeText: 'relogio',
+            url: 'a.svg',
+          },
+        },
+      ],
+      metadata: {
+        background: false,
+        name: 'gallery',
+        section_id: 'gallery',
+      },
+    });
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-grid-image');
+    expect(data.sectionId).toBe('gallery');
+    expect(data.title).toBe('Gallery');
+    expect(data.description).toBe('Lorem');
+    expect(data.grid[0].srcImg).toBe('a.svg');
+    expect(data.grid[0].altText).toBe('relogio');
+  });
+});
